4899333a9cffbeb9e59dae846b2376c82bb63646
Matthew <Matthew@Matthews-MacBook-Pro-3.local>
2014-08-09 18:08:15 -0400

    Renamed file

---
+++ Doubled/DBGameInterfaceConfig.h
@@ -0,0 +1,16 @@
+//
+//  DBGameInterfaceConfig.h
+//  Doubled
+//
+//  Created by Matthew Remmel on 7/17/14.
+//  Copyright (c) 2014 Matthew Remmel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DBGameScene.h"
+
+@interface DBGameInterfaceConfig : NSObject
+
++ (void)setUpInterface:(DBGameScene *)scene;
+
+@end
