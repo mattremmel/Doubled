568aac7e4827d2f222d6ab69ed274c3e9b7a1fcc
Matthew <Matthew@eg-1053.eg-dom2.local>
2014-08-11 17:27:59 -0400

    Fixed typo in URL

--- Doubled/DBMainMenuViewController.m
+++ Doubled/DBMainMenuViewController.m
@@ -116,7 +116,7 @@
 
 - (IBAction)buttonRate:(id)sender
 {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"items-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=888958280&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=888958280&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8"]];
 }
 
 - (IBAction)buttonSendFeedback:(id)sender
