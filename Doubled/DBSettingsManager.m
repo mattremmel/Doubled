ec0e030466d4ea30d0f08f73e25e471e2241ed9b
Matthew <Matthew@Matthews-MacBook-Pro-3.local>
2014-08-12 22:01:59 -0400

    Added reset settings function and moved around code to be more organized

--- Doubled/DBSettingsManager.m
+++ Doubled/DBSettingsManager.m
@@ -21,17 +21,22 @@
         isFirstLaunch = [[userSettings valueForKey:isFirstLaunchKey] boolValue];
         continuousSwipeEnabled = [[userSettings valueForKey:continuousSwipeEnabledKey] boolValue];
         gameCenterEnabled = [[userSettings valueForKey:gameCenterEnabledKey] boolValue];
+        touchTolerance = [[userSettings valueForKey:touchToleranceKey] integerValue];
+        if (touchTolerance == 0)
+        {
+            if (deviceType == iPadType)
+            {
+                touchTolerance = 60;
+            }
+            else
+            {
+                touchTolerance = 40;
+            }
+        }
     }
     else
     {
-        NSLog(@"SETT: Using default settings");
-        NSMutableDictionary *defaultSettings = [[NSMutableDictionary alloc] init];
-        [defaultSettings setValue:@YES forKey:isFirstLaunchKey];
-        [defaultSettings setValue:@YES forKey:continuousSwipeEnabledKey];
-        [defaultSettings setValue:@YES forKey:gameCenterEnabledKey];
-        [defaultSettings writeToFile:settingsFilePath atomically:true];
-        
-        [self loadSettings];
+        [self resetSettings];
     }
 }
 
@@ -44,6 +49,31 @@
     [userSettings setObject:[NSNumber numberWithBool:continuousSwipeEnabled] forKey:continuousSwipeEnabledKey];
     [userSettings setObject:[NSNumber numberWithBool:gameCenterEnabled] forKey:gameCenterEnabledKey];
     [userSettings writeToFile:settingsFilePath atomically:true];
+}
+
++ (void)resetSettings
+{
+    NSLog(@"SETT: Using default settings");
+    NSMutableDictionary *defaultSettings = [[NSMutableDictionary alloc] init];
+     NSString *settingsFilePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true)[0] stringByAppendingPathComponent:@"settings"];
+    [defaultSettings setValue:@YES forKey:isFirstLaunchKey];
+    [defaultSettings setValue:@YES forKey:continuousSwipeEnabledKey];
+    [defaultSettings setValue:@YES forKey:gameCenterEnabledKey];
+    
+    if (deviceType == iPadType)
+    {
+        [defaultSettings setValue:@60 forKey:touchToleranceKey];
+    }
+    else
+    {
+        [defaultSettings setValue:@40 forKey:touchToleranceKey];
+    }
+    
+    
+    
+    [defaultSettings writeToFile:settingsFilePath atomically:true];
+    
+    [self loadSettings];
 }
 
 @end
