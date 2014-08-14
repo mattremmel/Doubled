9f2769d0e980ed152282bddba6e646d6d1a7aab5
Matthew <Matthew@Matthews-MacBook-Pro-3.local>
2014-08-13 22:36:40 -0400

    Removed testing condition to prevent ads from loading

--- Doubled/DBGameViewController.m
+++ Doubled/DBGameViewController.m
@@ -194,8 +194,7 @@
             NSLog(@"GAD:  Requesting google ad");
             GADRequest *request = [GADRequest request];
             request.testDevices = @[ @"6c58f45e4871b58e5f70e8be96d2b96e", @"Simulator" ];
-            //[self.GADBannerView loadRequest: request];
-            [self adView:nil didFailToReceiveAdWithError:nil];
+            [self.GADBannerView loadRequest: request];
         }
     }
 }
