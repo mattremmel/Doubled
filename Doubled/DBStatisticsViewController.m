7377d7a909640ca980621372220d742422cdcf72
Matthew <matt.remmel@gmail.com>
2014-09-13 19:03:52 -0400

    Fixed creation credit in comments

--- Doubled/DBStatisticsViewController.m
+++ Doubled/DBStatisticsViewController.m
@@ -2,7 +2,7 @@
 //  DBStatisticsViewController.m
 //  Doubled
 //
-//  Created by Matthew on 8/12/14.
+//  Created by Matthew Remmel on 8/12/14.
 //  Copyright (c) 2014 Matthew Remmel. All rights reserved.
 //
 
